8399dba41aa7ef143a74dbd4dcc7781a30e34906
jhchoi1182 <jhchoi1182@gmail.com>
2023-07-25 23:46:48 +0900

    fix: 모달 컴포넌트와 createPortal의 렌더링 방식 일치하지 않던 문제 수정

--- src/components/atoms/modal/Modal.tsx
+++ src/components/atoms/modal/Modal.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useModalStore } from "@/libs/zustand";
-import React from "react";
+import { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 
 interface ModalProps {
@@ -9,15 +9,17 @@ interface ModalProps {
 }
 
 export function Portal({ children }: ModalProps) {
-  if (typeof window === "undefined") {
-    return null;
-  }
-  const node = document.getElementById("portal");
-  if (!node) {
-    return null;
+  const [element, setElement] = useState<HTMLElement | null>(null);
+
+  useEffect(() => {
+    setElement(document.getElementById("portal"));
+  }, []);
+
+  if (!element) {
+    return;
   }
 
-  return ReactDOM.createPortal(children, node);
+  return ReactDOM.createPortal(children, element);
 }
 
 interface ModalBackDropProps extends React.HTMLAttributes<HTMLDivElement> {
